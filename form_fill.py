d441690f86d6cc6edd0bc8acbbdce7c23e4e29e1
shin-kicker <106301653+shin-kicker@users.noreply.github.com>
2022-05-31 01:46:35 -0700

    Add files via upload

---
+++ form_fill.py
@@ -0,0 +1,50 @@
+import pdfrw
+
+
+
+ANNOT_KEY = '/Annots'
+ANNOT_FIELD_KEY = '/T'
+ANNOT_VAL_KEY = '/V'
+ANNOT_RECT_KEY = '/Rect'
+SUBTYPE_KEY = '/Subtype'
+WIDGET_SUBTYPE_KEY = '/Widget'
+
+
+def fill_pdf(input_pdf_path, output_pdf_path, data_dict):
+    template_pdf = pdfrw.PdfReader(input_pdf_path)
+    for page in template_pdf.pages:
+        annotations = page[ANNOT_KEY]
+        for annotation in annotations:
+            if annotation[SUBTYPE_KEY] == WIDGET_SUBTYPE_KEY:
+                if annotation[ANNOT_FIELD_KEY]:
+                    key = annotation[ANNOT_FIELD_KEY][1:-1]
+                    if key in data_dict.keys():
+                        if type(data_dict[key]) == bool:
+                            if data_dict[key] == True:
+                                annotation.update(pdfrw.PdfDict(
+                                    AS=pdfrw.PdfName('Yes')))
+                        else:
+                            annotation.update(
+                                pdfrw.PdfDict(V='{}'.format(data_dict[key]))
+                            )
+                            annotation.update(pdfrw.PdfDict(AP=''))
+    template_pdf.Root.AcroForm.update(pdfrw.PdfDict(NeedAppearances=pdfrw.PdfObject('true')))  # NEW
+    pdfrw.PdfWriter().write(output_pdf_path, template_pdf)
+    
+###########################
+#all the information that goes in the PDF
+
+if __name__ == '__main__':
+    pdf_template = "C:/Users/aaron/Documents/ATI/Concussion Project/Aether/form_experiment.pdf"
+    pdf_output = "C:/Users/aaron/Documents/ATI/Concussion Project/Aether/form_experiment_output.pdf"
+    
+
+    data_dict = {
+        'Name': 'Aaron Wu', 
+        'email': 'aaronwu@ucsb.edu',
+        'Sport': 'ATI',
+        'Check Box 1': True,
+        }
+
+    fill_pdf(pdf_template, pdf_output, data_dict)
+#############################
